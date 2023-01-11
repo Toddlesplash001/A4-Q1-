3b0e90b39017eea229c5d6daeff7e2430170664d
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2023-01-12 00:25:58 +0530

    Add files via upload

---
+++ A4 Q1.py
@@ -0,0 +1,13 @@
+i = int(input("Enter your marks "))
+if i<25:
+    print("Your grade is F")
+elif 25<=i<45:
+    print("Your grade is E")
+elif 45<=i<50:
+    print("Your grade is D")
+elif 50<=i<60:
+    print("Your grade is C")
+elif 60<=i<80:
+    print("Your grade is B")
+elif 80<=i<100:
+    print("Your grade is A")
